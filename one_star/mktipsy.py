b883609b18103271c3c6d5fcff015b7980eb8caa
Ben Keller <malzraa@gmail.com>
2016-06-24 16:58:58 -0400

    Use pytipsy rather than pynbody

--- one_star/mktipsy.py
+++ one_star/mktipsy.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
 import numpy as np
-import pynbody as pyn
-import matplotlib.pyplot as plt
+from pytipsy import wtipsy
 from sys import argv
+
 if __name__ == "__main__":
     infile = open('glass.dat').readlines()
-    tipsy = pyn.new(gas=len(infile))
-    bsize = 6
-    tipsy.gas['x'] = [bsize*(float(i.split()[0])-0.5) for i in infile]
-    tipsy.gas['y'] = [bsize*(float(i.split()[1])-0.5) for i in infile]
-    tipsy.gas['z'] = [bsize*(float(i.split()[2])-0.5) for i in infile]
-    tipsy.gas['temp'] = 1e-6
-    tipsy.gas['mass'] = 0.005461863242089748*64
-    tipsy.gas['metals'] = 0.0132*np.ones(len(infile))
-    tipsy.properties['a'] = 0
-    tipsy.write(filename="onestar.std", fmt=pyn.tipsy.TipsySnap)
+    bsize = float(argv[3])
+    h = {'time':0, 'n':len(infile)+1, 'ndim':3, 'ngas':len(infile), 'ndark':0, 'nstar':1}
+    gx = [bsize*(float(i.split()[0])-0.5) for i in infile]
+    gy = [bsize*(float(i.split()[1])-0.5) for i in infile]
+    gz = [bsize*(float(i.split()[2])-0.5) for i in infile]
+    gtemp = float(argv[4])*np.ones(len(infile))
+    gmass = float(argv[1])*np.ones(len(infile))
+    zeros = gmass*0
+    metals = 0.0132*np.ones(len(infile))
+    gas = {'mass':gmass, 'x':gx, 'y':gy, 'z':gz, 'vx':zeros, 'vy':zeros, 'vz':zeros, 
+        'dens':zeros, 'tempg':gtemp, 'h':zeros, 'zmetal':metals, 'phi':zeros}
+    stars = {'mass':[float(argv[2])], 'x':[0], 'y':[0], 'z':[0], 'vx':[0], 'vy':[0], 'vz':[0], 
+        'tform':[0], 'eps':[0], 'metals':[0.0132], 'phi':[0]}
+    wtipsy('onestar.std', h, gas, None, stars)
